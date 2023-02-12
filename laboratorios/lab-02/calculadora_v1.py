ab1aabdf6c986495bbc6aa79321165e8ebce4990
Lucas-Ribeir0 <lucasantonioribeiro0@gmail.com>
2023-02-12 08:32:50 -0300

    inicio capitulo 06
    principios de tratamento de arquivos

--- laboratorios/lab-02/calculadora_v1.py
+++ laboratorios/lab-02/calculadora_v1.py
@@ -1,47 +1,79 @@
 # Calculadora em Python
 
-# Desenvolva uma calculadora em Python com tudo que você aprendeu nos capítulos até aqui no curso. 
+# Desenvolva uma calculadora em Python com tudo que você aprendeu nos capítulos até aqui no curso.
 # A solução será apresentada no próximo capítulo!
 
 def soma(x, y):
     print(f"A soma entre os números {x} e {y} é de {x + y}")
 
-def subtracao (x, y):
+
+def subtracao(x, y):
     print(f"A subtração entre os números {x} e {y} é de {x - y}")
 
-def multiplicacao (x, y):
+
+def multiplicacao(x, y):
     print(f"A multiplicação entre os números {x} e {y} é de {x * y}")
 
-def divisao (x, y):
+
+def divisao(x, y):
     print(f"A divisão entre os números {x} e {y} é de {x / y}")
+
 
 print("\n******************* Calculadora em Python *******************")
 
 escolha = int(input("Seja bem-vindo a calculadora básica em Python!\n\n Escolha o modo de cálculo: \n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão \n\n Faça sua escolha: "))
 
-if escolha == 1:
-    x = float(input("Digite o primeiro número: "))
-    y = float(input("Digite o segundo número: "))
+# if escolha == 1:
+#     x = float(input("Digite o primeiro número: "))
+#     y = float(input("Digite o segundo número: "))
 
-    soma(x, y)
+#     soma(x, y)
 
-elif escolha == 2:
-    x = float(input("Digite o primeiro número: "))
-    y = float(input("Digite o segundo número: "))
+# elif escolha == 2:
+#     x = float(input("Digite o primeiro número: "))
+#     y = float(input("Digite o segundo número: "))
 
-    subtracao(x, y)
+#     subtracao(x, y)
 
-elif escolha == 3:
-    x = float(input("Digite o primeiro número: "))
-    y = float(input("Digite o segundo número: "))
+# elif escolha == 3:
+#     x = float(input("Digite o primeiro número: "))
+#     y = float(input("Digite o segundo número: "))
 
-    multiplicacao(x, y)
+#     multiplicacao(x, y)
 
-elif escolha == 4:
-    x = float(input("Digite o primeiro número: "))
-    y = float(input("Digite o segundo número: "))
-    
-    divisao(x, y)
+# elif escolha == 4:
+#     x = float(input("Digite o primeiro número: "))
+#     y = float(input("Digite o segundo número: "))
 
-else:
-    print("Opção inválida! Tente novamente!")
+#     divisao(x, y)
+
+# else:
+#     print("Opção inválida! Tente novamente!")
+
+  match escolha:
+       case 1:
+            x = float(input("Digite o primeiro número: "))
+            y = float(input("Digite o segundo número: "))
+
+            soma(x, y)
+
+        case 2:
+            x = float(input("Digite o primeiro número: "))
+            y = float(input("Digite o segundo número: "))
+
+            subtracao(x, y)
+
+        case 3:
+            x = float(input("Digite o primeiro número: "))
+            y = float(input("Digite o segundo número: "))
+
+            multiplicacao(x, y)
+
+        case 4:
+            x = float(input("Digite o primeiro número: "))
+            y = float(input("Digite o segundo número: "))
+
+            divisao(x, y)
+
+        case _:
+            print("Opção inválida! Tente Novamente")
